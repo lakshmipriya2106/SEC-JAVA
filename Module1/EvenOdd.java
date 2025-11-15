f3c38b8546b2dc27c3fb1e77a2900ddd49a71d1b
Lakshmi Priya R <rlakshmipriya152@gmail.com>
2025-11-15 23:03:50 +0530

    Add files via upload

--- Module1/EvenOdd.java
+++ Module1/EvenOdd.java
@@ -7,3 +7,19 @@ public class EvenOdd {
             System.out.println("Odd");
     }
 }
+
+// Import Scanner class
+import java.util.Scanner;
+
+class EvenOddInput {   // Removed 'public'
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);  
+        System.out.print("Enter a number: ");  
+        int num = sc.nextInt();  
+        if (num % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+        sc.close();
+    }
+}
